88d6dd5c1e8ed750e067e6095e98c226a1d59074
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-09-19 19:32:36 +0200

    Creación de clases MainActivity.kt y PrincipalActivity.kt

---
+++ app/src/main/java/com/example/taller1_assil/PrincipalActivity.kt
@@ -0,0 +1,14 @@
+package com.example.taller1_assil
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class PrincipalActivity {
+
+    class PrincipalActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_principal) // Asegúrate de tener un archivo XML llamado activity_principal.xml
+        }
+    }
+
+}
